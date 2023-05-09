640a867731f0c9fc87838057c23fc32d4f9960d2
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-09 16:58:49 +0300

    267. Catching error throw Operator

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Post} from "./post.model";
-import {map} from "rxjs/operators";
-import {Subject} from "rxjs";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Post } from "./post.model";
+import { map, catchError } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
 
 @Injectable({providedIn: "root"})
 export class PostService {
@@ -10,12 +10,13 @@ export class PostService {
 
   constructor(
     private http: HttpClient,
-  ) {}
+  ) {
+  }
 
-  createAndStorePost(title: string, content: string){
+  createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
     this.http.post<{ name: string }>('https://ng-complete-guide-nazarov-default-rtdb.firebaseio.com/posts.json',
-      postData )
+      postData)
       .subscribe(
         responseData => {
           console.log(responseData);
@@ -25,17 +26,22 @@ export class PostService {
       );
   }
 
-  fetchPost(){
+  fetchPost() {
     return this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-nazarov-default-rtdb.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({ ...responseData[key], id: key });
+      .pipe(
+        map(responseData => {
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({...responseData[key], id: key});
+            }
           }
-        }
-        return postArray;
-      }));
+          return postArray;
+        }),
+        catchError(errorRes => {
+          return throwError(errorRes);
+        })
+      );
   }
 
   deletePost() {
