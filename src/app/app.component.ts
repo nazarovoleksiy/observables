5d9d11fd632514a01d1c836a3dcb33e5dd644467
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-09 14:21:59 +0300

    260. Outputting Posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Post } from "./post.model";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(
     private http: HttpClient,
@@ -52,7 +52,8 @@ export class AppComponent implements OnInit {
       }))
       .subscribe(
         posts => {
-          console.log(posts)
+          this.loadedPosts = posts;
+          console.log(this.loadedPosts);
         }
       )
   }
