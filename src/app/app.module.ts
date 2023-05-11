d2e181bc1bb13fbe01127dff510ee19b2dcae210
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-11 11:37:57 +0300

    276. Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AuthInterceptorService} from "./auth-interceptor.service";
+import {LogginInterceptorService} from "./loggin-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,13 @@ import {AuthInterceptorService} from "./auth-interceptor.service";
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptorService,
     multi: true
-  }],
+  },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LogginInterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,22 +1,10 @@
-import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {tap} from "rxjs/operators";
+import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('request on way');
-    console.log(req.url);
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz')
     });
     return next.handle(modifiedRequest)
-      .pipe(tap(
-        event => {
-          console.log(event)
-          if (event.type === HttpEventType.Response) {
-            console.log(`Response arrived, body data`)
-            console.log(event.body)
-          }
-        }
-      ));
   }
 }
